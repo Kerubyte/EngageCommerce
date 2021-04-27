d60bf02932606b21447da76227897c48e2511460
Dawid Tyburek <davidtyburek@gmail.com>
2021-04-27 16:10:51 +0200

    Add repo for User database

---
+++ app/src/main/kotlin/com/example/engagecommerce/data/database/UserRepository.kt
@@ -0,0 +1,172 @@
+package com.example.engagecommerce.data.database
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.example.engagecommerce.application.repo.UserDatabase
+import com.example.engagecommerce.data.database.mappers.NullableInputUserEntityMapper
+import com.example.engagecommerce.data.database.mappers.NullableOutputUserEntityMapper
+import com.example.engagecommerce.data.entity.UserEntity
+import com.example.engagecommerce.domain.model.Product
+import com.example.engagecommerce.domain.model.User
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.DocumentReference
+import com.google.firebase.firestore.FieldValue
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.ktx.Firebase
+import com.user.sdk.UserCom
+import com.user.sdk.customer.Customer
+import com.user.sdk.customer.CustomerUpdateCallback
+import javax.inject.Inject
+
+class UserRepository
+@Inject constructor(
+    private val firestore: FirebaseFirestore,
+    private val auth: FirebaseAuth,
+    private val userCom: UserCom,
+    private val customerUpdateCallback: CustomerUpdateCallback,
+    private val inputUserMapper: NullableInputUserEntityMapper,
+    private val outputUserMapper: NullableOutputUserEntityMapper
+) : UserDatabase {
+
+    private val _navigate = MutableLiveData<Boolean>()
+    val navigate: LiveData<Boolean>
+        get() = _navigate
+
+    override fun getCurrentUser(): DocumentReference? {
+        if (auth.currentUser != null) {
+            return firestore.collection("users")
+                .document(auth.currentUser!!.uid)
+        }
+        return null
+    }
+
+    override fun getUserData(): LiveData<User>? {
+        val responseResult = MutableLiveData<User>()
+
+        if (auth.currentUser != null) {
+            val uid = auth.currentUser?.uid
+
+            firestore.collection("users")
+                .document(uid!!)
+                .get()
+                .addOnSuccessListener {
+                    val userEntity = it.toObject(UserEntity::class.java)
+                    val user = inputUserMapper.mapFromEntity(userEntity!!)
+                    responseResult.value = user
+                }
+            return responseResult
+        }
+        return null
+    }
+
+    override fun createNewUser(user: User) {
+        val userEntity = outputUserMapper.mapToEntity(user)
+
+        firestore.collection("users")
+            .document(user.uid)
+            .set(userEntity)
+            .addOnSuccessListener {
+                Log.d("createUser", "success")
+            }
+            .addOnFailureListener { e ->
+
+                Log.d("createUser", e.toString())
+            }
+    }
+
+    override fun addToCart(productUid: String) {
+        firestore.collection("users")
+            .document(auth.currentUser?.uid!!)
+            .update("cart", FieldValue.arrayUnion(productUid))
+            .addOnSuccessListener {
+                Log.d("cart", "Added to Cart")
+            }
+            .addOnFailureListener {
+                Log.d("cart", it.toString())
+            }
+    }
+
+    override fun removeFromCart(product: Product) {
+        firestore.collection("users")
+            .document(auth.currentUser?.uid!!)
+            .update("cart",FieldValue.arrayRemove(product.uid))
+            .addOnSuccessListener {
+                Log.d("cart", "Removed from Cart")
+            }
+            .addOnFailureListener {
+                Log.d("cart", it.toString())
+            }
+    }
+
+    override fun clearUserCart() {
+        firestore.collection("users")
+            .document(auth.currentUser?.uid!!)
+            .update(
+                mapOf(
+                    "cart" to FieldValue.delete()
+                )
+            )
+    }
+
+    override fun createAccount(email: String, password: String, firstName: String, lastName: String) {
+
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnSuccessListener {
+                val user = User(
+                    auth.currentUser!!.uid,
+                    firstName = firstName,
+                    lastName = lastName,
+                    auth.currentUser?.email!!,
+                    listOf()
+                )
+                val customer = Customer()
+                    .id(auth.currentUser!!.uid)
+                    .firstName(firstName)
+                    .lastName(lastName)
+                    .email(email)
+
+                createNewUser(user)
+                userCom
+                    .register(customer, customerUpdateCallback)
+
+                _navigate.value = true
+            }
+            .addOnFailureListener { exc ->
+                Log.d("register", exc.toString())
+            }
+    }
+
+    override fun loginUser(email: String, password: String) {
+
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnSuccessListener {
+                val customer = Customer()
+                    .id(auth.currentUser!!.uid)
+                    .email(email)
+
+                userCom
+                    .register(customer, customerUpdateCallback)
+
+                _navigate.value = true
+            }
+            .addOnFailureListener { exc ->
+                Log.d("login", exc.toString())
+            }
+    }
+
+    override fun signOut() {
+        Firebase.auth.signOut()
+        userCom.logout()
+        _navigate.value = true
+    }
+
+    override fun onDoneNavigating() {
+        _navigate.value = false
+    }
+
+    init {
+        _navigate.value = false
+    }
+}
