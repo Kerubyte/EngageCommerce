d6a3f10c707245d735d3a40765838c9d18199f12
Kerubyte <kerubyte@gmail.com>
2021-08-06 01:11:20 +0200

    Add Input Validator

---
+++ app/src/main/kotlin/com/kerubyte/engagecommerce/application/utils/auth/InputValidator.kt
@@ -0,0 +1,34 @@
+package com.kerubyte.engagecommerce.application.utils.auth
+
+import java.util.regex.Pattern
+
+object InputValidator {
+
+    /**
+     * the input is not valid if...
+     * ...email is not in correct format
+     * ...email is empty
+     * ...password is less than 6 chars
+     * ...firstName is less than 3 chars
+     * ...lastName is less than 3 chars
+     */
+
+    fun isValidEmail(email: String): Boolean {
+        return Pattern.matches(
+            "[a-zA-Z0-9\\+\\.\\_\\%\\-\\+]{1,256}" +
+                    "\\@" +
+                    "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,64}" +
+                    "(" +
+                    "\\." +
+                    "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,25}" +
+                    ")+",
+            email
+        )
+    }
+
+    fun isValidPassword(password: String) = password.length >= 6
+
+    fun isValidFirstName(firstName: String) = firstName.length >= 3
+
+    fun isValidLastName(lastName: String) = lastName.length >= 3
+}
---
+++ app/src/test/kotlin/com/kerubyte/engagecommerce/application/utils/auth/InputValidatorTest.kt
@@ -0,0 +1,63 @@
+package com.kerubyte.engagecommerce.application.utils.auth
+
+import com.google.common.truth.Truth
+import org.junit.Test
+
+class InputValidatorTest {
+
+    @Test
+    fun `empty email returns false`() {
+        val result = InputValidator.isValidEmail(
+            ""
+        )
+        Truth.assertThat(result).isFalse()
+    }
+
+    @Test
+    fun `incorrectly formatted email returns false`() {
+        val result = InputValidator.isValidEmail(
+            "hellogoogle@"
+        )
+        Truth.assertThat(result).isFalse()
+    }
+
+    @Test
+    fun `correctly formatted email returns true`() {
+        val result = InputValidator.isValidEmail(
+            "dawid@user.com"
+        )
+        Truth.assertThat(result).isTrue()
+    }
+
+    @Test
+    fun `password less than 6 chars returns false`() {
+        val result = InputValidator.isValidPassword(
+            ""
+        )
+        Truth.assertThat(result).isFalse()
+    }
+
+    @Test
+    fun `password with at least 6 chars returns true`() {
+        val result = InputValidator.isValidPassword(
+            "123456"
+        )
+        Truth.assertThat(result).isTrue()
+    }
+
+    @Test
+    fun `firstName with at least 3 chars returns true`() {
+        val result = InputValidator.isValidFirstName(
+            "abc"
+        )
+        Truth.assertThat(result).isTrue()
+    }
+
+    @Test
+    fun `lastName with at least 3 chars returns true`() {
+        val result = InputValidator.isValidLastName(
+            "abc"
+        )
+        Truth.assertThat(result).isTrue()
+    }
+}
