84479d1c03e289773fab5e02d885d854cb2b089c
Kerubyte <kerubyte@gmail.com>
2021-08-15 21:58:54 +0200

    Refactor DI modules

--- app/src/main/kotlin/com/kerubyte/engagecommerce/infrastructure/di/UtilModule.kt
+++ app/src/main/kotlin/com/kerubyte/engagecommerce/infrastructure/di/UtilModule.kt
@@ -1,6 +1,11 @@
 package com.kerubyte.engagecommerce.infrastructure.di
 
 import com.kerubyte.engagecommerce.application.utils.PriceFormatter
+import com.kerubyte.engagecommerce.application.utils.auth.InputValidator
+import com.kerubyte.engagecommerce.application.utils.mapper.User.NullableDatabaseUserMapper
+import com.kerubyte.engagecommerce.application.utils.mapper.User.NullableInputDatabaseUserMapper
+import com.kerubyte.engagecommerce.domain.model.database.DatabaseUser
+import com.kerubyte.engagecommerce.domain.model.local.User
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -14,4 +19,13 @@ object UtilModule {
     @Singleton
     @Provides
     fun providePriceFormatter(): PriceFormatter = PriceFormatter()
+
+    @Singleton
+    @Provides
+    fun provideInputValidator(): InputValidator = InputValidator
+
+    @Singleton
+    @Provides
+    fun provideNullableInputDatabaseUserMapper():
+            NullableDatabaseUserMapper<DatabaseUser, User> = NullableInputDatabaseUserMapper()
 }
