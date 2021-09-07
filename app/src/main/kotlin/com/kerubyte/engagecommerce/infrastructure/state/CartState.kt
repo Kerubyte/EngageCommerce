a5d4393fbd689a47a2b1c9dd05fbb31d0849c924
Kerubyte <kerubyte@gmail.com>
2021-09-07 21:18:47 +0200

    Remove unused CartState sealed class

--- app/src/main/kotlin/com/kerubyte/engagecommerce/infrastructure/state/CartState.kt
+++
@@ -1,8 +0,0 @@
-package com.kerubyte.engagecommerce.infrastructure.state
-
-sealed class CartState {
-
-    object Empty: CartState()
-    data class NotEmpty(val list: List<String>): CartState()
-    object Error: CartState()
-}
