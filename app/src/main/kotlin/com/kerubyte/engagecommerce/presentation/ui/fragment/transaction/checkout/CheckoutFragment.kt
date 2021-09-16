f9a5ad1fe53f94e7629b6a1df67a65087aab1622
Kerubyte <kerubyte@gmail.com>
2021-09-16 23:27:46 +0200

    Add Checkout

---
+++ app/src/main/kotlin/com/kerubyte/engagecommerce/presentation/ui/fragment/transaction/checkout/CheckoutFragment.kt
@@ -0,0 +1,8 @@
+package com.kerubyte.engagecommerce.presentation.ui.fragment.transaction.checkout
+
+import com.kerubyte.engagecommerce.presentation.ui.RootFragment
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class CheckoutFragment : RootFragment() {
+}
---
+++ app/src/main/kotlin/com/kerubyte/engagecommerce/presentation/ui/fragment/transaction/checkout/CheckoutFragmentViewModel.kt
@@ -0,0 +1,11 @@
+package com.kerubyte.engagecommerce.presentation.ui.fragment.transaction.checkout
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class CheckoutFragmentViewModel
+    @Inject
+    constructor(): ViewModel() {
+}
