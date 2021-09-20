c26655a2299148dd736c7406bcd0f8acb8bbc811
Kerubyte <kerubyte@gmail.com>
2021-09-20 23:47:03 +0200

    Refactor RepositoryModule.kt

--- app/src/main/kotlin/com/kerubyte/engagecommerce/infrastructure/di/RepositoryModule.kt
+++ app/src/main/kotlin/com/kerubyte/engagecommerce/infrastructure/di/RepositoryModule.kt
@@ -1,39 +1,28 @@
 package com.kerubyte.engagecommerce.infrastructure.di
 
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.firestore.FirebaseFirestore
-import com.kerubyte.engagecommerce.data.mapper.NullableInputDatabaseProductMapper
-import com.kerubyte.engagecommerce.data.mapper.user.NullableInputDatabaseUserMapper
-import com.kerubyte.engagecommerce.data.mapper.user.NullableOutputDatabaseUserMapper
 import com.kerubyte.engagecommerce.data.remote.ProductRepositoryImpl
 import com.kerubyte.engagecommerce.data.remote.UserRepositoryImpl
 import com.kerubyte.engagecommerce.domain.repo.ProductRepository
 import com.kerubyte.engagecommerce.domain.repo.UserRepository
+import dagger.Binds
 import dagger.Module
-import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-object RepositoryModule {
+abstract class RepositoryModule {
 
+    @Binds
     @Singleton
-    @Provides
-    fun provideProductRepository(
-        firestore: FirebaseFirestore,
-        inputDatabaseProductMapper: NullableInputDatabaseProductMapper
-    ): ProductRepository = ProductRepositoryImpl(firestore, inputDatabaseProductMapper)
+    abstract fun bindUserRepo(
+        userRepositoryImpl: UserRepositoryImpl
+    ): UserRepository
 
+    @Binds
     @Singleton
-    @Provides
-    fun provideUserRepository(
-        firestore: FirebaseFirestore,
-        firebaseAuth: FirebaseAuth,
-        inputDatabaseUserMapper: NullableInputDatabaseUserMapper,
-        outputDatabaseUserMapper: NullableOutputDatabaseUserMapper
-    ): UserRepository = UserRepositoryImpl(
-        firestore, firebaseAuth, inputDatabaseUserMapper, outputDatabaseUserMapper
-    )
+    abstract fun bindProductRepository(
+        productRepositoryImpl: ProductRepositoryImpl
+    ): ProductRepository
 }
