67f552bf65f4b4a7c78fd288a625dc080fc5d7ed
Kerubyte <kerubyte@gmail.com>
2021-09-21 22:42:43 +0200

    Add Extensions.kt

---
+++ app/src/main/kotlin/com/kerubyte/engagecommerce/infrastructure/util/Extensions.kt
@@ -0,0 +1,33 @@
+package com.kerubyte.engagecommerce.infrastructure.util
+
+import android.content.Intent
+import android.view.Gravity
+import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import com.google.android.material.transition.MaterialFadeThrough
+import com.google.android.material.transition.SlideDistanceProvider
+import com.kerubyte.engagecommerce.presentation.ui.activity.MainActivity
+
+fun Fragment.navigate(fragmentId: Int) {
+    Navigation.findNavController(requireView())
+        .navigate(
+            fragmentId
+        )
+}
+
+fun Fragment.setAnimation() {
+    enterTransition = MaterialFadeThrough().apply {
+        secondaryAnimatorProvider = SlideDistanceProvider(Gravity.END)
+    }
+
+    exitTransition = MaterialFadeThrough().apply {
+        secondaryAnimatorProvider = SlideDistanceProvider(Gravity.START)
+    }
+}
+
+fun Fragment.restartMainActivity() {
+    val intent = Intent(requireContext(), MainActivity::class.java).apply {
+        flags = (Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+    }
+    startActivity(intent)
+}
