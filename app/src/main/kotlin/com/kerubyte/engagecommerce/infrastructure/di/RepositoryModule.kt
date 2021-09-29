7f01bef4bd3d09c6ef3d86943921d4268e138c90
Kerubyte <kerubyte@gmail.com>
2021-09-29 22:08:54 +0200

    Change RepositoryModule scope and refactor

--- app/src/main/kotlin/com/kerubyte/engagecommerce/infrastructure/di/RepositoryModule.kt
+++ app/src/main/kotlin/com/kerubyte/engagecommerce/infrastructure/di/RepositoryModule.kt
@@ -1,28 +1,36 @@
 package com.kerubyte.engagecommerce.infrastructure.di
 
+import com.kerubyte.engagecommerce.data.remote.OrderRepositoryImpl
 import com.kerubyte.engagecommerce.data.remote.ProductRepositoryImpl
 import com.kerubyte.engagecommerce.data.remote.UserRepositoryImpl
+import com.kerubyte.engagecommerce.domain.repo.OrderRepository
 import com.kerubyte.engagecommerce.domain.repo.ProductRepository
 import com.kerubyte.engagecommerce.domain.repo.UserRepository
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
-import dagger.hilt.components.SingletonComponent
-import javax.inject.Singleton
+import dagger.hilt.android.components.ActivityRetainedComponent
+import dagger.hilt.android.scopes.ActivityRetainedScoped
 
 @Module
-@InstallIn(SingletonComponent::class)
+@InstallIn(ActivityRetainedComponent::class)
 abstract class RepositoryModule {
 
     @Binds
-    @Singleton
+    @ActivityRetainedScoped
     abstract fun bindUserRepo(
         userRepositoryImpl: UserRepositoryImpl
     ): UserRepository
 
     @Binds
-    @Singleton
+    @ActivityRetainedScoped
     abstract fun bindProductRepository(
         productRepositoryImpl: ProductRepositoryImpl
     ): ProductRepository
+
+    @Binds
+    @ActivityRetainedScoped
+    abstract fun bindOrderRepository(
+        orderRepositoryImpl: OrderRepositoryImpl
+    ): OrderRepository
 }
