5ed2924a3e1cb58ffafd2be3ed55b5068ed3bfee
Kerubyte <kerubyte@gmail.com>
2021-12-30 19:45:33 +0100

    Refactor PriceFormatter.kt to guard against incorrect input

--- app/src/main/kotlin/com/kerubyte/engagecommerce/infrastructure/util/PriceFormatter.kt
+++ app/src/main/kotlin/com/kerubyte/engagecommerce/infrastructure/util/PriceFormatter.kt
@@ -1,9 +1,19 @@
 package com.kerubyte.engagecommerce.infrastructure.util
 
 import android.icu.text.NumberFormat
+import com.kerubyte.engagecommerce.infrastructure.Constants.DEFAULT_CART_VALUE
+import java.util.*
 
 class PriceFormatter {
 
-    fun formatPrice(price: Double?): String = NumberFormat.getCurrencyInstance().format(price)
+    private val locale = Locale.UK
+    private val localisedFormatter = NumberFormat.getCurrencyInstance(locale)
 
+    fun formatPrice(price: Double?): String =
+
+        when (price) {
+
+            is Double -> localisedFormatter.format(price)
+            else -> localisedFormatter.format(DEFAULT_CART_VALUE)
+        }
 }
