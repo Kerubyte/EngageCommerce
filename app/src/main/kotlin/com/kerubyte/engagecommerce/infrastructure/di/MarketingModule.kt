aa387405b7d62e6effbf06f13803b46949fbd86a
Kerubyte <kerubyte@gmail.com>
2022-01-02 21:26:16 +0100

    Implement Marketing SDK

---
+++ app/src/main/kotlin/com/kerubyte/engagecommerce/infrastructure/di/MarketingModule.kt
@@ -0,0 +1,49 @@
+package com.kerubyte.engagecommerce.infrastructure.di
+
+import android.app.Application
+import android.util.Log
+import com.kerubyte.engagecommerce.BuildConfig
+import com.user.sdk.UserCom
+import com.user.sdk.customer.CustomerUpdateCallback
+import com.user.sdk.customer.RegisterResponse
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object MarketingModule {
+
+    @Provides
+    @Singleton
+    fun provideUserCom(application: Application): UserCom.Builder {
+
+       return UserCom.Builder(
+            application,
+            BuildConfig.SDK_TOKEN,
+            BuildConfig.API_KEY,
+            BuildConfig.APP_DOMAIN
+        )
+            .trackAllActivities(true)
+    }
+
+    @Provides
+    @Singleton
+    fun provideUserComInstance(): UserCom {
+
+        return UserCom.getInstance()
+    }
+
+    @Provides
+    fun provideObjectCustomerCallback() = object : CustomerUpdateCallback {
+        override fun onSuccess(p0: RegisterResponse) {
+            Log.d("register", "UserCom Registered")
+        }
+
+        override fun onFailure(p0: Throwable) {
+            Log.d("dupa", "UserCom Failure")
+        }
+    }
+}
--- app/src/main/kotlin/com/kerubyte/engagecommerce/presentation/ui/Application.kt
+++ app/src/main/kotlin/com/kerubyte/engagecommerce/presentation/ui/Application.kt
@@ -1,7 +1,19 @@
 package com.kerubyte.engagecommerce.presentation.ui
 
 import android.app.Application
+import com.user.sdk.UserCom
 import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
 
 @HiltAndroidApp
-class Application: Application()
+class Application: Application() {
+
+    @Inject
+    lateinit var provideUserCom: UserCom.Builder
+
+    override fun onCreate() {
+        super.onCreate()
+
+        provideUserCom.build()
+    }
+}
